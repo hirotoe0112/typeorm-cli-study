47a0039035bd7f6123efea6abd945bfcb1cdf28e
hirotoe0112 <hirotoe0112@gmail.com>
2023-03-26 15:49:53 +0900

    format

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,27 +1,21 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Material } from "./entity/Material"
-import { StudyRecord } from "./entity/StudyRecord"
-import { User } from "./entity/User"
-import { Initial1679808898602 } from "./migration/1679808898602-Initial"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Material } from "./entity/Material";
+import { StudyRecord } from "./entity/StudyRecord";
+import { User } from "./entity/User";
+import { Initial1679808898602 } from "./migration/1679808898602-Initial";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: false,
-    logging: true,
-    logger: 'file',
-    entities: [
-        User,
-        Material,
-        StudyRecord,
-    ],
-    migrations: [
-        Initial1679808898602,
-    ],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "test",
+  password: "test",
+  database: "test",
+  synchronize: false,
+  logging: true,
+  logger: "file",
+  entities: [User, Material, StudyRecord],
+  migrations: [Initial1679808898602],
+  subscribers: [],
+});
