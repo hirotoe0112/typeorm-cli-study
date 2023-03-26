3ba595caf389a4d1e0f9e7b293720346ec297c4d
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2023-03-26 16:04:39 +0900

    format

--- src/migration/1679813729460-Initial.ts
+++ src/migration/1679813729460-Initial.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Initial1679813729460 implements MigrationInterface {
-    name = 'Initial1679813729460'
+  name = "Initial1679813729460";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "study_record" ADD "date" TIMESTAMP`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "study_record" ADD "date" TIMESTAMP`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
+  }
 }
--- src/migration/1679814091170-Initial.ts
+++ src/migration/1679814091170-Initial.ts
@@ -1,16 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Initial1679814091170 implements MigrationInterface {
-    name = 'Initial1679814091170'
+  name = "Initial1679814091170";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
-        await queryRunner.query(`ALTER TABLE "study_record" ADD "date" date`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
+    await queryRunner.query(`ALTER TABLE "study_record" ADD "date" date`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
-        await queryRunner.query(`ALTER TABLE "study_record" ADD "date" TIMESTAMP`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "study_record" DROP COLUMN "date"`);
+    await queryRunner.query(`ALTER TABLE "study_record" ADD "date" TIMESTAMP`);
+  }
 }
